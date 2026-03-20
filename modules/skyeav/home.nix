cce7d54a68d6d29eb09a2a3e5e4b5670a0fda393
SkyeAv <skye.lane.goetz@gmail.com>
2026-03-19 22:52:39 -0700

    Created New Home And User Dirs

--- modules/skyeav/home.nix
+++ modules/skyeav/home.nix
@@ -1,5 +1,116 @@
 {
   pkgs,
-  inputs,
   ...
+}:
+{
+  # Home manager configuration
+  home-manager = {
+    useGlobalPkgs = true;
+    useUserPackages = true;
+    users.skyeav = {
+      home = {
+        # Do not modify this
+        stateVersion = "25.11";
+        # Append vars to path
+        sessionPath = [
+          "$HOME/.local/bin"
+          "$HOME/go/bin"
+        ];
+      };
+      programs = {
+        # Zsh configuration
+        zsh = {
+          enable = true;
+          enableCompletion = true;
+          autosuggestion.enable = true;
+          syntaxHighlighting.enable = true;
+          # Zsh aliases
+          shellAliases = {
+            amphetamine = ''systemd-inhibit --what=idle:sleep --why="Presentation" sleep infinity'';
+            rebuild = "sudo nixos-rebuild switch --flake /etc/nixos#skyeav";
+            skyemac = "ssh skyeav@192.168.1.6";
+            ps = "procs";
+            top = "htop";
+            du = "dust";
+            ls = "eza";
+            df = "duf";
+            cd = "z";
+          };
+          # Oh my zsh configuration
+          oh-my-zsh = {
+            enable = true;
+            plugins = [
+              "docker-compose"
+              "extract"
+              "docker"
+              "git"
+            ];
+            theme = "eastwood";
+            history.size = 100;
+          };
+        };
+        # Zoxide integration
+        zoxide = {
+          enable = true;
+          enableZshIntegration = true;
+        };
+        # Direnv integration
+        direnv = {
+          enable = true;
+          enableZshIntegration = true;
+        };
+        # Carapace integration
+        carapace = {
+          enable = true;
+          enableZshIntegration = true;
+        };
+        # Nushell
+        nushell.enable = true;
+        # Zed configuration
+        zed-editor = {
+          enable = true;
+          extensions = [
+            "toml"
+            "nix"
+            "dockerfile"
+            "proto"
+            "lua"
+            "elixir"
+            "latex"
+            "ruff"
+            "haskell"
+            "julia"
+            "nu"
+            "nim"
+            "perl"
+            "ocaml"
+            "github-actions"
+            "material-icon-theme"
+            "ayu-darker"
+          ];
+          installRemoteServer = true;
+          userSettings = {
+            theme = {
+              mode = "dark";
+              dark = "Ayu Darker";
+              terminal = {
+                shell = "system";
+              };
+            };
+          };
+        };
+        # Neovim configuration
+        neovim = {
+          enable = true;
+          viAlias = true;
+          vimAlias = true;
+        };
+        # Kitty configuration
+        kitty = {
+          enable = true;
+          shellIntegration.enableZshIntegration = true;
+        };
+      };
+    };
+  }
 }
---
+++ modules/skyeav/user.nix
@@ -0,0 +1,139 @@
+{
+  pkgs,
+  ...
+}:
+{
+  # NixOS user configuration
+  users.users.skyeav = {
+    isNormalUser = true;
+    shell = pkgs.zsh;
+    # Group configuration
+    extraGroups = [
+      "networkmanager"
+      "libvirtd"
+      "ydotool"
+      "render"
+      "podman"
+      "docker"
+      "wheel"
+      "input"
+      "video"
+      "audio"
+    ];
+    # Ranges for podman
+    subGidRanges = [
+      {
+        count = 65536;
+        startGid = 100000;
+      }
+    ];
+    subUidRanges = [
+      {
+        count = 65536;
+        startUid = 100000;
+      }
+    ];
+    # User package configuration
+    packages = with pkgs; [
+      beamMinimal28Packages.elixir_1_19
+      cudaPackages.cudatoolkit
+      nvtopPackages.nvidia
+      bitwarden-desktop
+      telegram-desktop
+      datafusion-cli
+      podman-compose
+      cabal-install
+      ghostscript
+      whisper-cpp
+      texliveFull
+      libreoffice
+      imagemagick
+      pavucontrol
+      aircrack-ng
+      antigravity
+      tor-browser
+      virt-viewer
+      micromamba
+      alsa-utils
+      metasploit
+      python314
+      nodejs_24
+      wireshark
+      fastfetch
+      geekbench
+      xdg-utils
+      streamrip
+      libnotify
+      opencode
+      qpwgraph
+      foremost
+      chemtool
+      mangohud
+      usbutils
+      pciutils
+      nix-diff
+      qrencode
+      ripgrep
+      zoom-us
+      vesktop
+      pyright
+      nixfmt
+      ffmpeg
+      libzip
+      duckdb
+      nimble
+      libgcc
+      psmisc
+      rustup
+      pdftk
+      rsync
+      hping
+      unzip
+      ocaml
+      slack
+      cmake
+      ninja
+      ngrok
+      wtype
+      procs
+      bisq2
+      ruff
+      nmap
+      file
+      tmux
+      htop
+      curl
+      opam
+      dune
+      wget
+      lshw
+      gawk
+      btop
+      dust
+      love
+      tree
+      prek
+      zlib
+      pipx
+      pip
+      ghc
+      duf
+      fzf
+      zip
+      gcc
+      git
+      eza
+      nim
+      bat
+      bun
+      lua
+      tor
+      dig
+      fd
+      jq
+      go
+      gh
+      uv
+    ];
+  };
+}
