2c6f22cbb93fc8cd9b44b4fdaa76899329e20d53
SkyeAv <skye.lane.goetz@gmail.com>
2026-03-19 21:38:39 -0700

    Started Modularization

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = ''
+    SL Goetz NixOS
+    skyeav@skyemac
+    26.05 (Yarara) x86_64
+    03-19-2026
+  '';
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nix-cachyos-kernel = {
+      url = "github:xddxdd/nix-cachyos-kernel/release";
+    };
+  };
+  outputs = inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      flake.nixosConfigurations.skyemac = inputs.nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = { inherit inputs; };
+        modules = [
+          ./modules/hardware.nix
+          ./modules/skyeav/user.nix
+          home-manager.nixosModules.home-manager
+          ./modules/skyeav/home.nix
+        ];
+      };
+    };
+}
