a1325cf404162a3b0fcfa860a2ff3d11f5a74140
SkyeAv <skye.a.goetz@gmail.com>
2026-08-03 11:02:05 -0700

    chore: deleted unused caches

--- modules/settings.nix
+++ modules/settings.nix
@@ -17,9 +17,7 @@
         "https://cuda-maintainers.cachix.org"
         "https://cache.flox.dev"
         "https://attic.xuyh0120.win/lantian"
-        "https://comfyui.cachix.org"
         "https://nix-community.cachix.org"
-        "https://cache.garnix.io"
       ];
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
@@ -27,9 +25,7 @@
         "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
         "flox-cache-public-1:7F4OyH7ZCnFhcze3fJdfyXYLQw/aV7GEed86nQ7IsOs"
         "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
-        "comfyui.cachix.org-1:33mf9VzoIjzVbp0zwj+fT51HG0y31ZTK3nzYZAX0rec="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
       ];
     };
     # Garbage collection
